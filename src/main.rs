a8af45d52a08f32eb7016406409ffe898b11e208
kumar10248 <kumardevashish000@gmail.com>
2026-07-20 04:01:35 +0530

    refactor: comment out example code and retain function for printing names

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,92 @@
 fn main() {
-    println!("Hello, world!");
+    // let name="Kumar Devashish";
+    // let age=12;
+    // println!("My name is {} and age is {}",name,age);
+
+    // println!("Hello, world!");
+    // let mut score:i32=100;
+    
+    //arithmetic operator
+
+    // score=138;
+    // println!("{score}");
+    // let a:i16=38;
+    // let b:i16=34;
+    // println!("{}",a+b);
+    // println!("{}",a-b);
+    // println!("{}",a*b);
+    // println!("{}",a/b);
+    // println!("{}",a%b);
+
+    //datatypes
+
+    // let mut name = String::from("Kumar");
+    // name.push_str(" Devashish");
+    // let age: u8 = 22;
+    // let cgpa: f32 = 7.9;
+    // let passed = true;
+    // let grade = 'A';
+    // let arr=[1,2,3,4];
+
+    // println!("ages: {age}");
+    // println!("cgpas: {cgpa}");
+    // println!("passeds: {passed}");
+    // println!("grades: {grade}");
+    // println!("Name: {name}");
+    // println!("{:?}",arr);
+
+//shadowing
+// let x=5;
+// let x=x+10;
+// println!("{x}");
+
+//Ownership
+
+//     let a = "Hello";
+//     let b = a;
+
+//     println!("{b}");
+//     println!("{a}");
+
+//     let x = 100;
+// let y = x;
+
+// println!("{x}");
+// println!("{y}");
+
+// let s1 = String::from("Rust");
+// let s2 = s1;
+
+// println!("{s1}");
+// println!("{s2}");
+
+//  ownership in function
+
+// let s = String::from("Rust");
+
+//     print_name(s);
+//     println!("{s}");
+
+// borrowing(reference) in function
+let s = String::from("Rust");
+
+    print_name(&s);
+    println!("{s}");
+
+
+
 }
+
+
+// fn print_name(name: String) {
+//     println!("{name}");
+// }
+
+
+fn print_name(name: &String) {
+    println!("{name}");
+}
+
+
+
+
