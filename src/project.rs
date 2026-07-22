5dc87fbcff21b3b284ba1fbff3faed38f8136ed5
kumar10248 <kumardevashish000@gmail.com>
2026-07-22 20:28:12 +0530

    feat: add uid field to Student struct and implement search methods by name and UID

--- src/project.rs
+++ src/project.rs
@@ -1,5 +1,6 @@
 struct Student{
     name:String,
+    uid:String,
     age:u8,
     cgpa:f32,
 
@@ -15,13 +16,38 @@ impl Student {
 
     fn display(&self){
        
-    println!("Student name is: {} , age is {} and CGPA is {}",
+    println!("Student name is: {} ,uid is {}, age is {} and CGPA is {}",
     self.name,
+    self.uid,
     self.age, 
     self.cgpa
 );
 
 
+    }
+    fn search_student<'a>(
+        students:&'a Vec<Student>,
+        name:&str,
+    )-> Option<&'a  Student>{
+
+        for student in students{
+            if student.name==name{
+                return Some(student);
+            }
+        }
+        None
+    }
+
+    fn search_student_byuid<'a>(
+        students:&'a Vec<Student>,
+        uid:&str,
+    )->Option<&'a Student>{
+ for student in students{
+            if student.uid==uid{
+                return Some(student);
+            }
+        }
+        None
     }
 }
 
@@ -30,26 +56,61 @@ fn main(){
 
 let stud1=Student{
     name:String::from("Devashish"),
+    uid:String::from("22BCS10248"),
     age:22,
     cgpa:7.90,
 };
 
 let stud2=Student{
     name:String::from("Harsha"),
+    uid:String::from("22BCS10131"),
     age:21,
     cgpa:8.70,
 };
+
+let stud3=Student{
+    name:String::from("Saniya"),
+    uid:String::from("22BCS10177"),
+    age:22,
+    cgpa:8.0,
+};
 students.push(stud1);
 students.push(stud2);
+students.push(stud3);
 
 for student in &mut students {
     student.change_cgpa();
 }
 
-for student in &students {
-   student.display();
+// for student in &students {
+//    student.display();
+// }
+
+let name=String::from("Saniya");
+let uid=String::from("22BCS10131");
+let result1=Student::search_student(&students,&name);
+let result2=Student::search_student_byuid(&students,&uid);
+
+match result1{
+    Some(student)=>{
+        println!("Found!!!🥳🥳🥳 ");
+        student.display();
+    }
+    None =>{
+         println!("Student not found with name {}",name);
+    }
 }
 
+
+match result2{
+    Some(student)=>{
+        println!("Found!!!🥳🥳🥳 ");
+        student.display();
+    }
+    None =>{
+         println!("Student not found with UID {}",uid);
+    }
+}
 
 
 
