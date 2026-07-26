88f67aa7cde501e66aebb75a91ba624101ddf213
kumar10248 <kumardevashish000@gmail.com>
2026-07-27 02:08:44 +0530

    refactor: update input prompt handling in read_string function and remove commented-out code

--- src/add.rs
+++ src/add.rs
@@ -1,9 +1,10 @@
-// use text_io::read;
 use std::io::{self, Write};
 
 
 pub fn read_string(prompt: &str) -> String {
-    println!("{prompt}: ");
+    print!("{prompt}: ");
+    io::stdout().flush().unwrap(); 
+
     let mut input = String::new();
     
     io::stdin()
@@ -29,23 +30,3 @@ pub fn read_int(prompt: &str) -> i32 {
         }
     }
 }
-
-// fn main(){
-//     let  a:i32;
-//     let  b:i32;
-//     println!("Enter two numbers to add using text_io crate");
-//     println!("Enter a number");
-//     a=read!();
-//     println!("Enter another number");
-//     b=read!();
-//     println!("Sum of {} and {} is {}",a,b,a+b);
-
-//     println!("Enter two numbers to add using std crate");
-
-//     let num1=read_int("Enter a number");
-//     let num2=read_int("Enter another number");
-//     println!("Sum of {} and {} is {}",num1,num2,num1+num2);
-
-
-
-// }
